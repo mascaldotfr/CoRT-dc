bb72c400094410175eec46ae4cd3b2d2fe463649
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-05-05 23:53:12 +0200

    fix minor typo in mydiscord

--- mydiscord.py
+++ mydiscord.py
@@ -9,7 +9,7 @@ import secrets
 
 client = discord.Client(intents=discord.Intents.default())
 
-def send_and_publish(target="test", msg ="message"):
+def send_and_publish(target="test", msg="message"):
     @client.event
     async def on_ready():
         channel = client.get_channel(secrets.channels[target])
