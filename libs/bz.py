f90a3925e66e88ac0158002497160e554bc4929d
mascal <116852972+mascaldotfr@users.noreply.github.com>
2024-06-02 11:58:15 +0200

    BZ: Ensure there will be BZs for 2 days when announcing the last BZ of the day

--- libs/bz.py
+++ libs/bz.py
@@ -35,6 +35,7 @@ def run():
     current_day = now.weekday()
     current_hour = now.hour
     tomorrow = current_day + 1 if current_day + 1 <= 6 else 0
+
     # get current bz status
     for hour in range(0, len(bz_begin[current_day])):
         if (current_hour >= bz_begin[current_day][hour] and
@@ -44,24 +45,32 @@ def run():
             bz_ends_at = bz_ends_at.replace(hour=bz_end[current_day][hour],
                                             minute=0, second=0)
             break
+
     # compute future bzs
-    for day in [current_day, tomorrow]:
+    check_bzs_days = [current_day, tomorrow]
+    day_offset = 0
+    # Ensure there will be BZs for 2 days when announcing the last BZ of the day
+    if current_hour >= bz_begin[current_day][-1]:
+        day_after_tomorrow = tomorrow + 1 if tomorrow + 1 <= 6 else 0
+        check_bzs_days = [tomorrow, day_after_tomorrow]
+        day_offset = 1 # skip today
+    for day in check_bzs_days:
         for hour in range(0, len(bz_begin[day])):
-            # skip passed BZ of th day
+            # skip passed BZ of the day
             if day == current_day and bz_begin[day][hour] <= current_hour:
                 continue
-            day_offset = 0 if day == current_day else 1
             time_holder = dt.now(tz=tz.utc) + timedelta(days=day_offset)
             next_bzs_begin.append(time_holder.replace(hour=bz_begin[day][hour],
                                                       minute=0, second=0))
             next_bzs_end.append(time_holder.replace(hour=bz_end[day][hour],
                                                     minute=0, second=0))
+        day_offset += 1
 
     storage = ConfigParser()
     storage.read("storage.ini")
     # ConfigParser can't deal with booleans
     str_bz_on = "1" if bz_on else "0"
-    # BZ status changed ? 
+    # BZ status changed ?
     if (storage.get("bz", "bz_on") != str_bz_on):
         storage.set("bz", "bz_on", str_bz_on)
         with open('storage.ini', 'w') as storagefile:
